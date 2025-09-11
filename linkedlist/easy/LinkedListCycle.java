8cc75df0872aa71075122457418cf05c4e22e723
dopesthetic <150394396+tyudope@users.noreply.github.com>
2025-09-11 22:41:04 +0200

    feat: Add Linked List Cycle solution (LeetCode 141)

---
+++ linkedlist/easy/LinkedListCycle.java
@@ -0,0 +1,40 @@
+package linkedlist.easy;
+
+import linkedlist.ListNode;
+
+
+import java.util.HashSet;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+    // Two pointer implementation.
+    public boolean hasCycleTwoPointer(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(slow != null && fast != null){
+            if (slow == fast){
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+
+    // HashSet implementation.
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> hashSet = new HashSet<>();
+        ListNode curr = head;
+        while(curr != null){
+            if (hashSet.contains(curr)){
+                return true;
+            }
+            hashSet.add(curr);
+            curr = curr.next;
+        }
+        return false;
+    }
+}
